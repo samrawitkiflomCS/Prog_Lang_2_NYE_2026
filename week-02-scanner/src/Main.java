a5c41ea583859fab7965561fbe444d6b89a1d8d2
samrawitkiflomCS <sapientia7sabedoria@gmail.com>
2026-02-28 12:59:24 +0100

    week_2 task done

--- week-02-scanner/src/Main.java
+++ week-02-scanner/src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+class Gallons{
+    public static void main(String args[]){
+        Scanner convert= new Scanner(System.in);
+        System.out.println("enter the amount in gallons");
+        float gallon= convert.nextFloat();
+        float gallon_To_Liters= gallon*3.78541f;
+        System.out.println("amount in liters= "+ gallon_To_Liters );
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
+
+
 }
